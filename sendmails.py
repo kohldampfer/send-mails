86bb9a7452dc6635d89a6853ed7c6aa5efeb5765
kohldampfer <kohldampfer@users.noreply.github.com>
2020-05-23 23:47:17 +0200

    Script should go on when one link does not work due an HTTPError exception.

--- sendmails.py
+++ sendmails.py
@@ -80,7 +80,10 @@ def getAllLinks(url):
     
         request = urllib.request.Request(url_to_parse,
             headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36' })
-        data = urllib.request.urlopen(request)
+        try:
+            data = urllib.request.urlopen(request)
+        except urllib.error.HTTPError:
+            return
         content = data.read()
         
         if domain == "www.youtube.com" or domain == "youtube.com":
